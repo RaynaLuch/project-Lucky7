ea300c683f5bffaf401b2bc0c5644055b41166b5
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2025-06-26 15:44:33 +0300

    Create project

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+dotenv.config();
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
