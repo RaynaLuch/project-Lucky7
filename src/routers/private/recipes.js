d5d20287a69f43d7dcf4b234b018d1254cf67024
Imojenga <aina.jenko@gmail.com>
2025-06-27 14:49:42 +0300

    add get-own-recipes route

--- src/routers/private/recipes.js
+++ src/routers/private/recipes.js
@@ -4,12 +4,16 @@ const auth = require('../../middlewares/authenticate');
 const {
   addRecipeToFavorites,
   getFavoriteRecipes,
+  getOwnRecipesController,
 } = require('../../controllers/recipesController.js');
+const ctrlWrapper = require('../../utils/ctrlWrapper.js');
 
 router.use(auth);
 
 router.post('/favorites/:id', addRecipeToFavorites);
 
 router.get('/favorites', getFavoriteRecipes);
+
+router.get('/own', ctrlWrapper(getOwnRecipesController));
 
 module.exports = router;
