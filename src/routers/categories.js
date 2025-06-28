2edc4be88b9fec929e06de057b7ddcc922f75ec2
BoredDarkness <gwinnblaidd@gmail.com>
2025-06-28 02:29:49 +0200

    test loadup

--- src/routers/categories.js
+++ src/routers/categories.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { fetchCategories } from '../controllers/categoriesController.js';
 
 const router = express.Router();
+
+router.get('/', ctrlWrapper(fetchCategories));
 
 export default router;
