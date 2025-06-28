311d49a868d414cfedf2cf0cd48d75823c1c0464
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2025-06-28 17:43:33 +0300

    recipe by id

--- src/db/models/recipe.js
+++ src/db/models/recipe.js
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const recipeSchema = new Schema({
   name: String,
-  ingredients: [String],
+  ingredients: [{ id: String, measure: String }],
 });
 
 export const Recipe = model('recipes', recipeSchema);
--- src/services/recipesServices.js
+++ src/services/recipesServices.js
@@ -1,6 +1,18 @@
 import { Recipe } from '../db/models/recipe.js';
+import { Ingredient } from '../db/models/ingredient.js';
 
 export const getOwnRecipes = async (userId) => {
   const ownRecipes = await Recipe.find(userId);
   return ownRecipes;
+};
+
+export const getRecipeById = async (recipeId) => {
+  const recipeById = await Recipe.findById(recipeId);
+  const recipe = recipeById.toObject();
+  for (let ingredient of recipe.ingredients) {
+    const ingData = await Ingredient.findById(ingredient.id);
+    ingredient['name'] = ingData?.name;
+  }
+
+  return recipe;
 };
